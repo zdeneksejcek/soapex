6dbf2dc7a0ebb02cf1448ac41c3d47b1e9d430fc
Zdenek Sejcek <zdenek@sejcek.cz>
2019-08-23 09:06:19 +0200

    hex mix config

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Soapex.MixProject do
       version: "0.1.2",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -25,6 +27,19 @@ defmodule Soapex.MixProject do
       {:httpoison, "~> 1.5"},
       {:xml_builder, "~> 2.1"},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp description() do
+    "This library offers implementation of SOAP1.1 and SOAP1.2 client. It downloads WSDL ang generates proxy module."
+  end
+
+  defp package() do
+    [
+      name: "soapex",
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/zdeneksejcek/soapex"}
     ]
   end
 end
