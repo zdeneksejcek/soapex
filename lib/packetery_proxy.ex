01fe98eac287f3fcdf1949c1674e5250fd7a0377
Josef Strzibny <strzibny@strzibny.name>
2020-03-18 11:39:09 +0700

    Stop fetching soap.wsdl file on every compilation

--- lib/packetery_proxy.ex
+++ lib/packetery_proxy.ex
@@ -1,6 +1,6 @@
 defmodule PacketeryProxy do
-  @moduledoc false
+  # @moduledoc false
 
-  use Soapex.Proxy,
-    wsdl_path: "http://www.zasilkovna.cz/api/soap.wsdl"
+  # use Soapex.Proxy,
+  #  wsdl_path: "http://www.zasilkovna.cz/api/soap.wsdl"
 end
